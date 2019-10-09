63bb637f36d6e52d91214c844cd2ff0df5f6f337
stetter <guilherme_stetter@hotmail.com>
2019-10-09 10:51:54 -0300

    'teste extension gitHub'

--- public/src/controller/DropBoxController.js
+++ public/src/controller/DropBoxController.js
@@ -6,12 +6,12 @@ class DropBoxController{
         this.initEvents()
     }
     initEvents(){
-        this.btnSendFileEL.addEventListener('click', event =>{
+        this.btnSendFileEL.addEventListener('click', event =>{//eventos do botao
         this.inputFilesEl.click();
         });
         this.inputFilesEl.addEventListener('change', event =>{
             console.log(event.target.files)
-            this.snackModalEl.style.display = 'block';//exibir via css
+            this.snackModalEl.style.display = 'block';//exibir modal e barra de progresso
         });
     }
 }
