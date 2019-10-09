a6cac4ca89e7b831c582d995a09176a739b2f99f
stetter <guilherme_stetter@hotmail.com>
2019-10-09 15:22:04 -0300

    'configurando: ajax, promise.all, recebendo arquivos com node, formidable, atualizando view e barraga de progresso, tamplate tipos de arquivos'

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,23 @@
 var express = require('express');
 var router = express.Router();
-
+var formidable = require('formidable')//tratar arquivos
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+router.post('/upload', (req, res) =>{
+  let form = new formidable.IncomingForm({ //chamar formulario e fazer configs
+    uploadDir: './upload',
+    keepExtensions: true
+  });
+  form.parse(req, (err, fields, files) =>{//interpretar os dados que estao vindo
+    //separa em 2 json dados via post e arquivos
+    res.json({
+      files:files
+    });
+  });
+
+ 
+});
 module.exports = router;
